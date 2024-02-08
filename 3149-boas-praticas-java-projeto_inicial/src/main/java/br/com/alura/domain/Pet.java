47af9c932378879a840211f4fa01cc2c43371d19
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2024-02-08 17:28:01 -0300

    ClienthttpConf

--- 3149-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/domain/Pet.java
+++ 3149-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/domain/Pet.java
@@ -2,6 +2,9 @@ package br.com.alura.domain;
 
 public class Pet {
 
+    public Pet() {
+    }
+
     public Pet(String tipo, String nome, String raca, Integer idade, String cor, float peso) {
         this.tipo = tipo;
         this.nome = nome;
@@ -11,10 +14,46 @@ public class Pet {
         this.peso = peso;
     }
 
+    private Long id;
+
     private String tipo;
     private String nome;
     private String raca;
     private Integer idade;
     private String cor;
     private float peso;
+
+    private Abrigo abrigos[];
+
+    public Abrigo[] getAbrigos() {
+        return abrigos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
 }
