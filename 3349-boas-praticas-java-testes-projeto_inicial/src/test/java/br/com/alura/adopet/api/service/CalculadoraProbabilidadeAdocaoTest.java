c16f8ba169c9f229745b5dd9271da36c89cd9ccb
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2024-02-26 11:37:39 -0300

    Boas praticas em testes

--- 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
+++ 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -7,26 +7,31 @@ import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.model.ProbabilidadeAdocao;
 import br.com.alura.adopet.api.model.TipoPet;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraProbabilidadeAdocaoTest {
     @Test
+    @DisplayName("Teste probabilidade alta com idade e peso baixo")
     void testeProbabilidadeAlta() {
         //idade 4 anos e 4kg
+        //Arrange
         Abrigo abrigo = new Abrigo(new CadastroAbrigoDto("Abrigo feliz", "91 99999999", "abrigoFeliz@teste.com"));
         Pet pet = new Pet(new CadastroPetDto(TipoPet.GATO, "Miau", "Siamês", 4, "Cinza", 4.0f), abrigo);
-
         CalculadoraProbabilidadeAdocao calculadora = new CalculadoraProbabilidadeAdocao();
+        //act
         ProbabilidadeAdocao probabilidadeAdocao = calculadora.calcular(pet);
 
         //JUnit
         //Resposta esperada e De onde esperamos essa resposta
         //Probabilidade retornada da calculadora criada
+        //Assert
         Assertions.assertEquals(ProbabilidadeAdocao.ALTA, probabilidadeAdocao);
     }
     @Test
+    @DisplayName("Teste probabilidade media com idade alta e peso baixo")
     void testeProbabilidadeMedia(){
         //15 anos e 4kg
         Abrigo abrigo = new Abrigo(new CadastroAbrigoDto("Abrigo feliz", "91 99999999", "abrigoFeliz@teste.com"));
@@ -40,20 +45,4 @@ class CalculadoraProbabilidadeAdocaoTest {
         //Probabilidade retornada da calculadora criada
         Assertions.assertEquals(ProbabilidadeAdocao.MEDIA, probabilidadeAdocao);
     }
-
-    @Test
-    void testeProbabilidadeBaixa(){
-        //15 anos e
-        Abrigo abrigo = new Abrigo(new CadastroAbrigoDto("Abrigo feliz", "91 99999999", "abrigoFeliz@teste.com"));
-        Pet pet = new Pet(new CadastroPetDto(TipoPet.GATO, "Miau", "Siamês", 4, "Cinza", 4.0f), abrigo);
-
-        CalculadoraProbabilidadeAdocao calculadora = new CalculadoraProbabilidadeAdocao();
-        ProbabilidadeAdocao probabilidadeAdocao = calculadora.calcular(pet);
-
-        //JUnit
-        //Resposta esperada e De onde esperamos essa resposta
-        //Probabilidade retornada da calculadora criada
-        Assertions.assertEquals(ProbabilidadeAdocao.ALTA, probabilidadeAdocao);
-    }
-
 }
