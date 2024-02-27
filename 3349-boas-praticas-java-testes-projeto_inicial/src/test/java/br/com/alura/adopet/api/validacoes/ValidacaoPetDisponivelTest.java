81ddffb157899507da7c7cdff43deaebe6b91679
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2024-02-27 13:58:23 -0300

    Teste com mockito

---
+++ 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
@@ -0,0 +1,40 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class ValidacaoPetDisponivelTest {
+    //Dentro dessa classe possui classes com mock
+    @InjectMocks
+    private ValidacaoPetDisponivel validacao;
+    @Mock
+    private PetRepository petRepository;
+
+    @Mock
+    private Pet pet;
+
+    @Test
+    @DisplayName("Deveria permitir adoção do pet")
+    void validacao() {
+        SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(7l, 2l, "qualquer motivo");
+        //Transformar em atributo
+        //ValidacaoPetDisponivel validacao = new ValidacaoPetDisponivel();
+
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(false);
+
+
+        Assertions.assertDoesNotThrow(() -> validacao.validar(dto));
+    }
+}
